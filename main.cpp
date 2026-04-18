b0ae681dadcbdfb0d67e20042e47d5c552049cfe
Harshit Agrawal <harshitagrawal2004@gmail.com>
2026-04-18 20:54:05 +0530

    Initial commit: Setup project structure and ASCII ramp logic

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define STB_IMAGE_IMPLEMENTATION  // tells stb to include the actual code
+#include "library/stb_image.h"
+
+using namespace std;
+
+
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "enter the file path \n";
+        return 1;
+    }
+
+}
