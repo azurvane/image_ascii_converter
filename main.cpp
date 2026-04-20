bc6d20498420c1dea6f07577c9fae4655eec50b7
Harshit Agrawal <harshitagrawal2004@gmail.com>
2026-04-20 16:43:31 +0530

    added path safety check
    added a safety check in main.cpp to check if the image path is provided or not. if not then through a error message

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@ using namespace std;
 
 
 int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "image path is not provided\n";
+        return 1;
+    }
     Convertor convertor(argv[1]);
     convertor.imageAscii();
 }
